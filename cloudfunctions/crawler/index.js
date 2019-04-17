363a44df11571185556aec65d16e2b8dc789eed4
jiemicc <1543088706@qq.com>
2019-04-17 10:06:43 +0800

    start

--- cloudfunctions/crawler/index.js
+++ cloudfunctions/crawler/index.js
@@ -1,194 +1,284 @@
-// 云函数入口文件
-const cloud = require('wx-server-sdk')
-const puppeteer = require('puppeteer')
+// const puppeteer = require('puppeteer')
 
-const url = 'https://movie.douban.com/explore#!type=movie&tag=%E8%B1%86%E7%93%A3%E9%AB%98%E5%88%86&sort=recommend&page_limit=20&page_start=0'
-let sleep = time => {
-  return new Promise(resolve => {
-    setTimeout(resolve, time)
-  })
-}
-
-// 云函数入口函数
- ;(async (event, context) => {
-
-
-  console.log(" start visit target page");
-
-  const brower = await puppeteer.launch({
-    args: ["--no-sandbox"]
-
-  })
-
-  const page = await brower.newPage()
-
-  await page.goto(url, {
-    waitUntil: 'networkidle0'
-  })
-
-  await page.waitForSelector('.more')
-
-  for (let i = 0; i < 1; i++) {
-    await sleep(2000)
-    page.click('.more')
-
-  }
-
-  const res = await page.evaluate(() => {
-
-    var $ = window.$
-    var items = $('.list-wp .item')
-
-    var links = []
-    if (items.length > 0) {
-      items.each((index, el) => {
-        var el = $(el)
-
-        var doubanId = el.find('.cover-wp').data('id')
-        var rate = el.find('strong').text()
-        var contentLink = el.attr('href')
+const qiniu = require('qiniu')
+const fs = require('fs')
+const nanoid = require('nanoid')
+const config = require('./config')
 
 
 
+const bucket = config.qiniu.bucket
+ ;(async () => {
+  const mac = new qiniu.auth.digest.Mac(config.qiniu.AK, config.qiniu.SK)
 
+  const cfg = new qiniu.conf.Config()
+  const client = new qiniu.rs.BucketManager(mac, cfg)
 
-        links.push({
-          doubanId,
-          rate,
-          contentLink
-        })
-      })
-    }
-
-    return links
-  })
-
-
-
-
-  if (res && res.length > 0) {
-    res.forEach(targetEl => {
-      
-      ;(async () =>  {
-        const doubanId = targetEl.doubanId
-        console.log(doubanId);
-        
-        let trailerLink 
-        const contentPage = await brower.newPage()
-        const  targetLink = "https://movie.douban.com/subject/" + doubanId+"/"
- 
-        await contentPage.goto(targetLink, {
-          waitUntil: "networkidle0"
-        })
-        console.log(123);
-        
-        return
-        await sleep(1000)
-       
-        
-        await contentPage.evaluate(() => {
-          var $ = window.$
-          var content = $('#content')
-          
-          trailerLink = $('.related-pic-video').attr('href')
-          if (!trailerLink) return false
-
-          targetEl.name = content.find('h1 span:first-child').text()
-          targetEl.wantCount = content.find('.subject-others-interests-ft a:last-child').text()
-          targetEl.type = content.find('span[property="v:genre"]').text()
-          targetEl.actor = []
-
-          // 获取演员
-          var celebritys = targetEl.find('.celebrities-list .celebrity')
-          celebritys.each((index, celebritysEl) => {
-            celebritysEl = $(celebritysEl)
-            var name = celebritysEl.find('.name a').text()
-            var avaterBg = celebritysEl.find('.avater').css('background-image')
-            var avater = avater.subString(avaterBg.indexOf('(') + 1, avaterBg.indexOf(')'))
-
-            targetEl.actor.push({
-              name,
-              avater
-            })
-
-
-          })
-
-
-          
-
-        })()
-        console.log(targetEl);
-        
-        
-        
-        contentPage.close()
-
-        // 获取电影预告片
-        const trailerpage = await brower.newPage()
-        await trailerpage.goto(trailerLink, {
-          waitUntil: "networkidle0"
-        })
-
-        await trailerpage.evaluate(() => {
-          var $ = window.$
-          var trailerUrl = $('#movie_player source').attr('src')
-
-          targetEl.trailerUrl = trailerUrl
-
-
-        })
-        trailerpage.close()
-
-        // 获取海报封面图
-      
-        const posterUrl = "https://movie.douban.com/subject/" + doubanId + "/photos?type=R"
-        const posterPage = await brower.newPage()
-
-        await posterPage.goto(posterUrl, {
-          waitUntil: 'networkidle0'
-        })
-
-        await posterPage.evaluate(() => {
-          var $ = window.$
-          var posterWrappers = $('.poster-col3 li')
-          if (posterWrappers.length > 0) {
-            posterWrappers.each((index, posterEl) => {
-              posterEl = $(posterEl)
-              var mixSpec = posterEl.find('.prop')
-              var specArray = mixSpec.split('x')
-              if (specArray[0] / specArray[1] > 1.5 && specArray[0] / specArray[1] > 2.5) {
-                targetEl.poster = posterEl.find('.cover img').attr('src')
-              }
-            })
+  const uploadToQiniu = (url, key) => {
+    return new Promise((reslove, reject) => {
+      client.fetch(url, bucket, key, (err, ret, info) => {
+        if (err) {
+          reject(err)
+        } else {
+          if (info.statusCode == 200) {
+            reslove(key)
+          } else {
+            reject(info)
           }
-
-
-        })
-
-        posterPage.close()
-
-        
-
-
-
-
-
-
-      })()
-
-
+        }
+      })
     })
   }
 
 
+  const url = 'https://movie.douban.com/explore#!type=movie&tag=%E8%B1%86%E7%93%A3%E9%AB%98%E5%88%86&sort=recommend&page_limit=20&page_start=0'
+  let sleep = (time) => {
+    return new Promise(resolve => {
+      setTimeout(resolve, time)
+    })
+  }
+
+  const getMovies = async () => {
+    console.log(" start visit target page");
+
+    const brower = await puppeteer.launch({
+      args: ["--no-sandbox"],
+    })
+
+    const page = await brower.newPage()
+
+    await page.goto(url, {
+      waitUntil: 'networkidle0'
+    })
+
+    await page.waitForSelector('.more')
+
+    for(let i=0;i < 10;i++){
+        await sleep(2000)
+        page.click('.more')
+    }
+
+    const res = await page.evaluate(() => {
+
+      var $ = window.$
+      var items = $('.list-wp .item')
+
+      var links = []
+      if (items.length > 0) {
+        items.each((index, el) => {
+          var el = $(el)
+
+          var doubanId = el.find('.cover-wp').data('id')
+          var rate = el.find('strong').text()
+          var contentLink = el.attr('href')
 
 
 
 
 
+          links.push({
+            doubanId,
+            rate,
+            contentLink
+          })
+        })
+      }
+
+      return links
+    })
+    console.log('已获取爬取电影目录列表, 电影列表数目为' + res.length)
+    console.log(res);
+    
+    if (res && res.length > 0) {
+
+      let k = 0
+      const len = res.length
+      const getMovieDetail = async (index, targetEl) => {
+
+        const doubanId = targetEl.doubanId
+        const contentPage = await brower.newPage()
+
+        await contentPage.goto(targetEl.contentLink, {
+          waitUntil: 'domcontentloaded',
+          timeout: 0
+        })
+
+
+        const other = await contentPage.evaluate(() => {
+
+          var $ = window.$
+          var content = $('#content')
+
+          let trailerLink = $('.related-pic-video').attr('href')
+          if (!trailerLink) return false
+
+          let name = content.find('h1 span:first-child').text()
+          let wantCount = parseInt(content.find('.subject-others-interests-ft a:last-child').text())
+          let type = content.find('span[property="v:genre"]').text()
+          let actor = []
+
+          // 获取演员
+          var celebritys = content.find('.celebrities-list .celebrity')
+          celebritys.each((index, celebritysEl) => {
+            celebritysEl = $(celebritysEl)
+            var name = celebritysEl.find('.name a').text()
+            var avatarBg = celebritysEl.find('.avatar').css('backgroundImage')
+            var bgLen = avatarBg.length
+            var avatar = avatarBg.substring(avatarBg.lastIndexOf('"', bgLen - 5) + 1, avatarBg.lastIndexOf('"'))
+            const celebritysItem = {
+              name,
+              avatar
+            }
+            actor.push(celebritysItem)
+
+
+          })
+
+          return {
+            name,
+            wantCount,
+            type,
+            actor,
+            trailerLink
+          }
+
+        })
+
+
+        targetEl.name = other.name
+        targetEl.wantCount = other.wantCount
+        targetEl.type = other.type
+        targetEl.actor = other.actor
+        targetEl.trailerLink = other.trailerLink
+        contentPage.close()
+
+
+        // 获取电影预告片
+        const trailerpage = await brower.newPage()
+        await trailerpage.goto(targetEl.trailerLink, {
+          timeout: 0,
+          waitUntil: 'domcontentloaded'
+        })
+        const trailer = await trailerpage.evaluate(() => {
+          var $ = window.$
+          var trailerUrl = $('#movie_player source').attr('src')
+
+
+          return trailerUrl
+        })
+        trailerpage.close()
+
+
+        targetEl.trailer = trailer
+
+        //获取海报
+        const posterPage = await brower.newPage()
+        const posterLink = "https://movie.douban.com/subject/" + doubanId + "/photos?type=R"
+        await posterPage.goto(posterLink, {
+          timeout: 0,
+          waitUntil: 'domcontentloaded'
+        })
+
+        const poster = await posterPage.evaluate(() => {
+          var $ = window.$
+
+          var posterContent = $('#content')
+
+          var posterItems = posterContent.find('.poster-col3 li')
+
+          const nearRatio = 2
+          let minNearRatio = 2
+          let minNearIndex = 0
 
 
 
+          posterItems.each((posterIndex, posterEl) => {
+
+
+            posterEl = $(posterEl)
+
+            const propStr = posterEl.find('.prop').text().replace(/[\n\s]/g, '')
+
+            const propArr = propStr.split('x')
+
+            if (Math.abs(propArr[0] / propArr[1] - nearRatio) < minNearRatio) {
+              minNearRatio = Math.abs(propArr[0] / propArr[1] - nearRatio)
+              minNearIndex = posterIndex
+            }
+
+
+
+          })
+          const poster = $('.poster-col3 li:eq(' + minNearIndex + ') img').attr('src')
+
+          return poster
+        })
+        targetEl.poster = poster
+        posterPage.close()
+        delete targetEl.contentLink
+        delete targetEl.trailerLink
+
+        return targetEl
+      }
+
+      while (k < len) {
+        console.log('正在爬取第' + (k + 1) + '项数据')
+        try{
+          await getMovieDetail(k, res[k])
+        }catch( err ){
+          console.log(err);
+          
+        }
+        k++
+      }
+    }
+    return res
+  }
+
+
+
+
+  const movieData = await getMovies()
+  
+  let k = 0
+  const len = movieData.length
+  const upLoadItem = async k => {
+    console.log(k);
+    
+    let nanoId = nanoid()
+    await uploadToQiniu(movieData[k].trailer, nanoId + '.mp4')
+    movieData[k].trailer = 'http://ppbde1o09.bkt.clouddn.com/' + nanoId + '.mp4'
+
+    nanoId = nanoid()
+    await uploadToQiniu(movieData[k].poster, nanoId + '.webp')
+    movieData[k].poster = 'http://ppbde1o09.bkt.clouddn.com/' + nanoId + '.webp'
+
+    for (let i = 0; i < movieData[k].actor.length; i++) {
+      nanoId = nanoid()
+      await uploadToQiniu(movieData[k].actor[i].avatar, nanoId + '.webp')
+      movieData[k].actor[i].avatar = 'http://ppbde1o09.bkt.clouddn.com/' + nanoId + '.webp'
+    }
+    console.log("成功上传第"+(k+1)+"条数据");
+    
+  }
+  while (k < len) {
+   try{
+    await upLoadItem(k)
+   
+   }catch(err){
+    movieData.splice(k,1)
+     console.log(err);
+     
+   }
+   k++
+  
+  }
+   fs.writeFile('movieHot.json',JSON.stringify(movieData),(err) => {
+     if( err ) throw err
+     console.log('电影数据写入成功');
+     
+   })
+
+ 
 
 })()
--- cloudfunctions/crawler/qiniu.js
+++ cloudfunctions/crawler/qiniu.js
@@ -12,7 +12,7 @@ const mac =  new qiniu.auth.digest.Mac(config.qiniu.AK,config.qiniu.SK)
 const cfg =  new qiniu.conf.Config()
 const client  =  new qiniu.rs.BucketManager( mac , cfg )
 
-const uploadToQiniu = async ( url,key ) =>{
+exports.uploadToQiniu =  ( url,key ) =>{
     return new Promise( (reslove,reject) => {
         client.fetch( url,bucket,key,( err ,ret ,info ) => {
              if( err ){
@@ -28,24 +28,7 @@ const uploadToQiniu = async ( url,key ) =>{
     } )
 }
 
-;(async () => {
-   let movies=[{
-       video:'http://vt1.doubanio.com/201904021357/a147ab2a9b8c21a947f5eef6e21ed1d1/view/movie/M/402440451.mp4'
-   }]
-movies.map( async movie => {
-    if( movie.video && !movie.key ){
-        try {
-            let video = await uploadToQiniu( movie.video, nanoid()+'.mp4' )
 
-            console.log(video);
-            
-        } catch (error) {
-            console.log(error);
-            
-        }
-    }
-} )
 
-})()
 
 
